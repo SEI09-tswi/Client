720c5ae165ae8be552997b8af3554c427a47f219
jenniferpayano <arlenepayano@gmail.com>
2020-04-22 12:00:21 -0400

    Starting socket.io

--- assets/scripts/messages/ui.js
+++ assets/scripts/messages/ui.js
@@ -9,6 +9,7 @@ const sendFailure = function (data) {
 }
 
 const getMsgSuccessfully = function (data) {
+
   for (let i = 0; i < data.chats.length; i++) {
     $('#messages').append('<li>' + data.chats[i].message + '</li>')
   }
@@ -18,9 +19,19 @@ const getMsgFailure = function (data) {
   $('#message').text('Error while getting message')
 }
 
+const displayMessages = function (message) {
+  console.log('in display msg')
+  store.data.messages.push(message)
+  const object = {message}
+  console.log(object)
+  object.current_user_id = store.user._id
+  $('#messages').append('<li>' + object + '</li>')
+}
+
 module.exports = {
   sendSuccessfully,
   sendFailure,
   getMsgSuccessfully,
-  getMsgFailure
+  getMsgFailure,
+  displayMessages
 }
