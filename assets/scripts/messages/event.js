99f74f64e0a268aa37c927e420af66f1bad51e3f
nicksolie <nick.solie1@gmail.com>
2020-04-23 17:43:45 -0400

    Started working on update button

--- assets/scripts/messages/event.js
+++ assets/scripts/messages/event.js
@@ -2,12 +2,10 @@ const getFormFields = require('./../../../lib/get-form-fields')
 const ui = require('./ui')
 const api = require('./api')
 const store = require('../store')
-
-
 const io = require('socket.io-client')
 const socket = io.connect('http://localhost:4741')
 
-const displayMassage = function() {
+const displayMassage = function () {
   api.onGetMessage()
     .then(ui.getMsgSuccessfully)
     .catch(ui.getMsgFailure)
@@ -16,7 +14,6 @@ const displayMassage = function() {
 // Delete chat message
 const ondelete = function (event) {
   const id = $(event.target).data('_id')
-  console.log('id v8888 ' + id)
   api.removeMessage(id)
     .then(function () {
       socket.emit('chat message', 'send')
@@ -27,10 +24,10 @@ const ondelete = function (event) {
     .catch(function () {
       console.log('fail')
     })
-    socket.emit('chat message', 'send')
-    socket.on('chat message', function (msg) {
-      ui.getMsgSuccessfully(msg)
-    })
+  socket.emit('chat message', 'send')
+  socket.on('chat message', function (msg) {
+    ui.getMsgSuccessfully(msg)
+  })
 }
 
 const onSend = function (event) {
@@ -47,24 +44,24 @@ const onSend = function (event) {
     .catch(ui.sendFailure)
 }
 
-
-
-
-const onupdate = function(event) {
-  const data = {
-    "chat": {
-      "message": "change"
-    }
-  }
-  const id = $(event.target).data('id')
-  console.log('id ' + id)
+// Update chat
+const onupdate = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  console.log(data)
+  // const data = {
+  //   'chat': {
+  //     'message': '22'
+  //   }
+  // }
+  console.log(data)
+  const id = $(event.target).data('_id')
+  console.log(id)
   api.updateMessage(data, id)
-    .then(function() {
-      console.log('updateMessage suces')
+    .then(function () {
       displayMassage()
     })
 }
-
 
 module.exports = {
   onSend,
